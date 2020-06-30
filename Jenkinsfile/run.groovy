4c4fd480880e4bf9291134b7c864d0b7f62370af
zaiceva-m <67535267+zaiceva-m@users.noreply.github.com>
2020-06-30 18:11:09 +0500

    Update run.groovy

--- Jenkinsfile/run.groovy
+++ Jenkinsfile/run.groovy
@@ -46,6 +46,19 @@ pipeline {
             }
         }
         
+        stage ('Stop'){
+            steps{
+                script{
+                    withCredentials([
+                        usernamePassword(credentialsId: 'srv_sudo',
+                        usernameVariable: 'username',
+                        passwordVariable: 'password')
+                    ]) {
+                            sh "echo '${password}' | sudo -S docker stop mz"
+                    }
+                }
+            }
+        
         stage ('Write result in the file'){
             steps{
                 script{
